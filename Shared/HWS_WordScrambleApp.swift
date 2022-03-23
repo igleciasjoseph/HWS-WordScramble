3f95cd229afeac3c24cb1828f59d165c3596c497
Joseph Iglecias <igleciasjoseph@gmail.com>
2022-03-22 22:30:31 -0400

    Initial Commit

---
+++ Shared/HWS_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_WordScrambleApp.swift
+//  Shared
+//
+//  Created by Joseph Iglecias on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct HWS_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
